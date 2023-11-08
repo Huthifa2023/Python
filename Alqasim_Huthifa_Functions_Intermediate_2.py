f273398cc1cd00fcb133db7c30b7aed1fc3bf063
Huthifa Alqasim <huthifa2013@gmail.com>
2023-11-08 16:27:16 +0200

    Alqasim_Huthifa_Functions_Intermediate_2

---
+++ Alqasim_Huthifa_Functions_Intermediate_2.py
@@ -0,0 +1,97 @@
+# x = [ [5,2,3], [10,8,9] ] 
+# students = [
+#     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#     {'first_name' : 'John', 'last_name' : 'Rosales'}
+# ]
+# sports_directory = {
+#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# }
+# z = [ {'x': 10, 'y': 20} ]
+
+# Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+# x[1][0] = 15
+# print(x)
+
+# Change the last_name of the first student from 'Jordan' to 'Bryant'
+# students[0]['last_name'] = 'Bryant'
+# print(students)
+
+# In the sports_directory, change 'Messi' to 'Andres'
+# sports_directory['soccer'][0] = 'Andres'
+# print(sports_directory)
+
+# Change the value 20 in z to 30
+# z[0]['y']=30
+# print(z)
+
+
+############################################################################################################
+# def iterateDictionary(list):
+#     for i in list:
+#         print('first_name'+' - ' +i['first_name'] + ', - last_name - '+ i['last_name'])
+
+# students = [
+#     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#     {'first_name' : 'John', 'last_name' : 'Rosales'},
+#     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#     ]
+# iterateDictionary(students)
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+
+###################################################################################################################
+# students = [
+#     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#     {'first_name' : 'John', 'last_name' : 'Rosales'},
+#     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+#     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+#     ]
+
+# def iterateDictionary2(key_name, some_list):
+#     for i in some_list:
+#         print(i[key_name])
+
+# iterateDictionary2('last_name',students)
+##################################################################################################################
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    print(len(some_dict['locations']) , 'locations')
+    for i in some_dict['locations']:
+        print(i)
+        
+    print('\n\n', len(some_dict['instructors']) , 'instructors')
+    for j in some_dict['instructors']:
+        print(j)
+
+printInfo(dojo)
+
+# # output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
