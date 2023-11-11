eabf1e4468e56728587da4f4f1bba8491fc248dc
Huthifa Alqasim <huthifa2013@gmail.com>
2023-11-11 16:29:54 +0200

    Hello_flask/hello.py

--- flask/flask_fundamentals/Hello_flask/hello.py
+++ flask/flask_fundamentals/Hello_flask/hello.py
@@ -2,7 +2,17 @@ from flask import Flask  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return 'Hello World! this is default route'  # Return the string 'Hello World!' as a response
+@app.route('/ok')
+def ok_page():
+    return 'this same web server but another page, using ok route'
+@app.route('/success')
+def success():
+    return 'success'
+@app.route('/hi/<name>')
+def hi_name(name):
+    print(name)
+    return('Hi ' + name)
+
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
-
