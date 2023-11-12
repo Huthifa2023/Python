3fd6b7e4e4072abc0ca94484bc7268b7706d0b8b
Huthifa Alqasim <huthifa2013@gmail.com>
2023-11-12 14:44:29 +0200

    Insertion_Sort_Optional_Assignment

--- _python/python_fundementals/Insertion_Sort_Optional_Assignment.py
+++ _python/python_fundementals/Insertion_Sort_Optional_Assignment.py
@@ -1,20 +1,28 @@
-#insertion sort, works by shifiting the value to left, until it becomes sorted relative to all left values
-#list = [3,5,7,9,0,2,1,4,6,8]
+#Insertion sort is a simple sorting algorithm that works similar to the
+#way you sort playing cards in your hands. The array is virtually split into 
+#a sorted and an unsorted part. Values from the unsorted part are picked and 
+#placed at the correct position in the sorted part.
 
-#pseudo code 
-#suppose idex0 is smallest value
-#grap index1 and compare it with idex0
-#if second value smaller than first value shift, 
-#now compair 3rd value with the two left values
-#and shift in right place
 
+#To sort an array of size N in ascending order iterate over the array 
+#and compare the current element (hold) to its predecessor, if the key 
+#element is smaller than its predecessor, compare it to the elements 
+#before. Move the greater elements one position up to make space for 
+#the swapped element.
 
 def insertion_sort(list):
+    if len(list) <= 1:
+        print("either list is empty or no need to sort!")
+    
     for i in range(1,len(list)):
-        summation = 0
-        for j in range(i,-1):
-            summation += list[j]
-        if list[i] < summation
+        j = i-1
+        hold = list[i]
+        while j>=0 and hold<list[j]:
+            list[j+1] = list[j]
+            j-=1
+        list[j+1] = hold
+        
+    return list
 
 
-print(insertion_sort([2,1]))
+print(insertion_sort([2,1,-1]))
