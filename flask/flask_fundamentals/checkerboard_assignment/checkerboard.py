9ee9e018f695b58ce4217b8bd85e223b7cbd86da
Huthifa Alqasim <huthifa2013@gmail.com>
2023-11-13 13:42:48 +0200

    checkerboard_assignment

--- flask/flask_fundamentals/checkerboard_assignment/checkerboard.py
+++ flask/flask_fundamentals/checkerboard_assignment/checkerboard.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template
-import math
 
 checkerboardAPP = Flask(__name__)
 
 @checkerboardAPP.route('/')
 def eight():
-    return render_template('checkerboard.html',rows=8, columns=8)
+    return render_template('checkerboard.html',rows=8, columns=8,colors=['red','black'])
 
 @checkerboardAPP.route('/<int:rows>')
 def input_rows(rows):
-    return render_template('checkerboard.html', rows=int(rows), columns=8)
+    return render_template('checkerboard.html', rows=int(rows), columns=8, colors=['red','black'])
 
 @checkerboardAPP.route('/<int:rows>/<int:columns>')
 def input_only(rows,columns):
-    return render_template('checkerboard.html',rows=int(rows), columns=int(columns))
+    return render_template('checkerboard.html',rows=int(rows), columns=int(columns), colors=['red','black'])
+
+@checkerboardAPP.route('/<int:rows>/<int:columns>/<color1>/<color2>')
+def input_with_colors(rows, columns, color1, color2):
+    arr = [color1,color2]
+    return render_template('checkerboard.html',rows=int(rows), columns=int(columns), colors=arr)
 
 @checkerboardAPP.errorhandler(404)
 def error_page(e):
@@ -21,4 +25,3 @@ def error_page(e):
 
 if __name__ == "__main__":
     checkerboardAPP.run(debug=True)
-
