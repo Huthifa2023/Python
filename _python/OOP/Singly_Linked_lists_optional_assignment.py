790ef6251b9c171c1313c15ad7d079d0e33b7395
Huthifa Alqasim <huthifa2013@gmail.com>
2023-11-14 15:35:43 +0200

    single linked list

--- _python/OOP/Singly_Linked_lists_optional_assignment.py
+++ _python/OOP/Singly_Linked_lists_optional_assignment.py
@@ -7,16 +7,44 @@ class Slist:
     def __init__(self):
         self.head = None
     
+    def add_to_start(self,val):
+            new_node = Node(val)
+            Current_head = self.head
+            new_node.next = Current_head
+            self.head = new_node
+            return self
+
     def add_to_end(self, value):
         if self.head == None:	
             self.head = Node(value)
-            return self	
+            return self
         new_node = Node(value)
         runner = self.head
         while (runner.next != None):
             runner = runner.next
         runner.next = new_node	
         return self
+
+
+
+
+
+    def add_at_index(self,position, value):
+
+        new_node = Node(value)
+        current_node = self.head
+        i = 0
+        while i < position+1:
+            prev_node = current_node
+            current_node = current_node.next
+            position -= 1
+            i+=1
+        prev_node.next = new_node
+        new_node.next = current_node
+        return self
+
+
+
 
     def delete_at_index(self,position):
         if position == 0:
@@ -44,8 +72,9 @@ class Slist:
     
 
 list0 = Slist()
-list0.add_to_end('huthifa').add_to_end('Ahmad').add_to_end('Ismael').add_to_end("Alqasim")
-list0.delete_at_index(0).print_values()
+list0.add_to_end(1).add_to_end(2).add_to_end(3).add_to_end(4)
+
+list0.add_at_index(2,43473).print_values()
 
 
 
@@ -71,12 +100,3 @@ list0.delete_at_index(0).print_values()
 
 
 
-
-
-
-# def add_to_front(self,val):
-#         new_node = Node(val)
-#         Current_head = self.head
-#         new_node.next = Current_head
-#         self.head = new_node
-#         return self
