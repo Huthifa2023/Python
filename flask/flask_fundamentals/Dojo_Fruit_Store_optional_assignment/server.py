059353d5f5979e27870869eb3e01f2acc32942ba
Huthifa Alqasim <huthifa2013@gmail.com>
2023-11-16 15:08:05 +0200

    add redirect

--- flask/flask_fundamentals/Dojo_Fruit_Store_optional_assignment/server.py
+++ flask/flask_fundamentals/Dojo_Fruit_Store_optional_assignment/server.py
@@ -1,23 +1,31 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
 
 @app.route('/')         
 def index():
     return render_template("index.html")
 
-@app.route('/checkout', methods=['POST'])         
-def checkout():
-    print(request.form)
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    id_id   =   request.form['student_id']
-    strawberry = request.form['strawberry']
-    Raspberry = request.form['raspberry']
-    Apple = request.form['apple']
-    total_orderd_fruits = int(strawberry)+int(Raspberry)+int(Apple)
-    return render_template("checkout.html", first_name = first_name, last_name=last_name,
-        id_id = id_id, strawberry=strawberry, Raspberry=Raspberry, Apple=Apple, 
-        total_orderd_fruits=total_orderd_fruits )
+
+@app.route('/send_data_to_backend', methods=['POST'])         
+def receive_data_from_frontEnd():
+    session['first_name'] = request.form['first_name']
+    session['last_name'] = request.form['last_name']
+    session['student_id']   =   request.form['student_id']
+    session['strawberry'] = request.form['strawberry']
+    session['raspberry'] = request.form['raspberry']
+    session['apple'] = request.form['apple']
+    session['total_orderd_fruits'] = int(session['strawberry'])+int(session['strawberry'])+int(session['apple'])
+    return redirect('/show')
+
+@app.route('/show')
+def show_only():
+    return render_template("checkout.html", first_name = session['first_name'], last_name=session['last_name'],
+        id_id = session['student_id'], strawberry=session['strawberry'], Raspberry=session['raspberry'], Apple=session['apple'], 
+        total_orderd_fruits=session['total_orderd_fruits'])
+
+
 
 @app.route('/fruits')         
 def fruits():
