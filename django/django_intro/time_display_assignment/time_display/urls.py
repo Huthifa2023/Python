711a8a74706c36fbd400636f99fe5767140f8a02
Huthifa Alqasim <huthifa2013@gmail.com>
2023-11-19 13:56:30 +0200

    added css style

--- django/django_intro/time_display_assignment/time_display/urls.py
+++ django/django_intro/time_display_assignment/time_display/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.current_time),
-    path('time_display', views.current_time),
+    path('time_display', views.redirect_to_root),
 ]
--- django/django_intro/time_display_assignment/time_display/views.py
+++ django/django_intro/time_display_assignment/time_display/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
 from time import gmtime, strftime
-
+from datetime import datetime
 
 def current_time(request):
-    time_now = strftime("%Y-%m-%d %H-%M-%p", gmtime())
-
+    time_GMT = datetime.utcnow()
+    time_GMT_2 = datetime.now()
     context = {
-        "time" : time_now,
+        "time_GMT" : time_GMT,
+        "time_GMT_2" : time_GMT_2,
     }
     return render(request, 'index.html', context)
 
 
+def redirect_to_root(requset):
+    return redirect(requset, '/')
