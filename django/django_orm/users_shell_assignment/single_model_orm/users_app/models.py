5d37fbd11567063eadaedd09d4a1a3ac4f462f93
Huthifa Alqasim <huthifa2013@gmail.com>
2023-11-20 22:37:44 +0200

    ORM_liteSQL_first_step

---
+++ django/django_orm/users_shell_assignment/single_model_orm/users_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class User(models.Model):
+    def __str__(self):
+        return f"<Movie object: {self.title} ({self.id})>"  #the shell to display multi objects correctly
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
