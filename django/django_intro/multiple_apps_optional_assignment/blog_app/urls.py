82d860fdd5726199d391090e3c903187bf3643ab
Huthifa Alqasim <huthifa2013@gmail.com>
2023-11-22 15:28:25 +0200

    multiple_apps_optional_assignment

---
+++ django/django_intro/multiple_apps_optional_assignment/blog_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('new', views.new),
+    path('create', views.create),
+    path('<number>', views.show),
+    path('<number>/edit', views.edit),
+    path('<number>/delete', views.destroy),
+]
---
+++ django/django_intro/multiple_apps_optional_assignment/surveys_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SurveysAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'surveys_app'
