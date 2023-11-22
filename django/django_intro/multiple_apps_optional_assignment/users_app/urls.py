b2204506b8123c3db6f7df35e7909bf34d48eeb4
Huthifa Alqasim <huthifa2013@gmail.com>
2023-11-22 15:45:26 +0200

    optional import method from another app

--- django/django_intro/multiple_apps_optional_assignment/users_app/urls.py
+++ django/django_intro/multiple_apps_optional_assignment/users_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+from blog_app.views import index       # to solve optional root route utilize the same method as the /blogs route
+
 urlpatterns = [
+    path('', index),                    #invoke index method located in blogs_app in views
     path('register', views.register),
     path('login', views.login),
     path('users/new', views.register),
