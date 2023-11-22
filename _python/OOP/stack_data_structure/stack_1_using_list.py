2f77deea8544e68cd1a45e849e7bd465ac8869a9
Huthifa Alqasim <huthifa2013@gmail.com>
2023-11-22 15:29:57 +0200

    stack_data_structure

--- _python/OOP/stack_data_structure/stack_1_using_list.py
+++ _python/OOP/stack_data_structure/stack_1_using_list.py
@@ -1,16 +1,34 @@
+#in this file, we built a stack (LIFO: last in first out) where we have only one pointer that is top, 
+#represent stack using list is easy, (but i think there is no point for this, cuz just use the list as it is, but ok for sake testing)
+
+
+
+class Stack:
     def __init__(self):
         self.list = []
 
     def push(self, val):
         self.list.append(val)
+        return self
 
     def pop(self):
         self.list.pop()
+        return self
         
 
     def top(self):
         top_value = self.list[-1]
-        return top_value
+        print(f"the top value fro this stack is:{top_value}")
+        return self
 
     def getMin(self):
-        return min(self.list)
+        print(f'minimum value in this stack is:{min(self.list)}')
+        return self
+    
+    def display_stack(self):
+        for i in self.list:
+            print(f'{i}',end='->')
+
+stack_1 = Stack()
+stack_1.push(0).push(1).push(2).push(3)
+stack_1.top()
--- _python/OOP/stack_data_structure/stack_2_using_linked_list.py
+++ _python/OOP/stack_data_structure/stack_2_using_linked_list.py
@@ -1,16 +1,54 @@
+#in this file we built stack using linked_list, which is more practical than building it using list i think, 
+#there is no stack like list in python, so the main idea is to make a stack using OOP
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+
+
+class Stack:
     def __init__(self):
-        self.list = []
-
-    def push(self, val):
-        self.list.append(val)
-
+        self.top = None
+    
+    def push(self,value):
+        new_node = Node(value)
+        if self.top is None:
+            self.top = new_node
+            return self
+        current = self.top
+        self.top = new_node
+        self.top.prev = current
+        return self
+    
     def pop(self):
-        self.list.pop()
-        
+        if self.top is None:
+            print('Empty Stack!')
+            return self
+        current = self.top
+        self.top = current.prev
+        return self
 
-    def top(self):
-        top_value = self.list[-1]
-        return top_value
+    def peak(self):
+        if self.top is None:
+            print('Empty Stack!')
+            return self
+        print(f'\n{self.top.value} is the top of this stack')
+        return self
+    
+    def display_elements(self):
+        if self.top is None:
+            print('Empty Stack!')
+            return self
+        runner = self.top
+        while runner is not None:
+            print(f'{runner.value}', end=' -> ')
+            runner = runner.prev
+        return self
+#############################################################################
 
-    def getMin(self):
-        return min(self.list)
+stack0 = Stack()
+stack0.push(11).push(33).push(66).push(77).push(99).pop().display_elements()
+stack0.peak()
+
+    
